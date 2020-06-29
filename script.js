fc5c954919fed22871d91d41c33faf409957dd04
The Crystal King <dylanawhite11@gmail.com>
2020-06-29 17:09:08 -0500

    declared global variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// Declare variables
+const container = document.getElementsByClassName("container");
+const text = document.getElementById("text");
+
+// Other times not hardcoded in case we want to change total breathing time
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
