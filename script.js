d23ad8ee2a592a84452330cd067756f69380104c
The Crystal King <dylanawhite11@gmail.com>
2020-06-29 17:24:43 -0500

    dynamically rendering breathing text on screen, moving to CSS next

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
+breatheAnimation();
+
 // Set container animation properties
 function breatheAnimation() {
   text.innerHTML = "Breathe in!";
@@ -21,3 +23,6 @@ function breatheAnimation() {
     }, holdTime);
   }, breatheTime);
 }
+
+// Run breatheAnimation every 7.5 seconds
+setInterval(breatheAnimation, totalTime);
