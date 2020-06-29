943635d4dfe98c474052d0f085c865fcdb20452f
The Crystal King <dylanawhite11@gmail.com>
2020-06-29 17:12:08 -0500

    starting breatheAnimation function, implementing grow class for CSS animation later on

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const text = document.getElementById("text");
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
+
+// Set container animation properties
+function breatheAnimation() {
+  text.innerHTML = "Breathe in!";
+  container.className = "container grow";
+}
